b7d1b78d736c65ddf80bc50f3da8dd31782a1022
Alonzo Jaymz <alonzozabel327@gmail.com>
2021-12-21 01:52:28 -0800

    Uploading completed course project.

---
+++ CodeBook.md
@@ -0,0 +1,26 @@
+---
+title: "CodeBook: Getting and Cleaning Data Course Project"
+author: "Alonzo"
+date: "12/21/2021"
+output: pdf_document
+---
+
+#BREAKDOWN
+This Code Book pertains to a tidy data set that takes from a broader data set. A group of 30 volunteers wore a smart phone that had an accelerometer and gyroscope. Measurements using the tools at time intervals were taken of these subjects and the activity they were performing at the time of each measurement was recorded. These activities were: walking, walking upstairs, walking downstairs, sitting, standing and laying. 561 features were recorded for a given observation. This data was tidied up, taking a subset of features measuring the mean and standard deviation. This data also added descriptive names to the variables. Columns were also added to sync the subject being tested and the activity being performed for a given observation with the rest of the data. This data were further simplified by taking the mean of the variable measurements, grouped by subject and activity.
+
+#CODEBOOK
+
+###Number of variables/columns: 68
+###Number of observations: 180 (30 subjects * 6 activities)
+
+##Variables/Columns
+Subject: 1 of the 30 subjects the given observation pertains to.
+Activity: The activity the subject was performing when the observation was recorded.
+The further 66 variables are the means and standard deviations data.
+
+##Observations/Rows
+Each row corresponds to a given subject and the activity they were performing for the given measurements.
+
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,48 @@
+library(data.table)
+library(dplyr)
+
+#Reads in training and test data
+testData <- data.table::fread("./UCI Har Dataset/test/X_test.txt", data.table=FALSE)
+trainData <- data.table::fread("./UCI Har Dataset/train/X_train.txt", data.table=FALSE)
+
+#ANSWER TO PART 1: mergedData
+#Merges training and test data sets into a single data set by row combing them
+mergedData <- rbind(testData,trainData)
+
+#ANSWER TO PART 2: extractedMeasurements
+features <- data.table::fread("./UCI Har Dataset/features.txt", data.table=FALSE)[,2] #reads in all of the features/variables
+featureIndices <- grep("(mean|std)\\(\\)", features) #finds indices of features/variables that are for either the mean or standard deviation of a measurement
+extractedFeatures <- mergedData[,featureIndices] #extracts only features/variables that are the mean or standard deviation of a measurement
+
+#ANSWER TO PART 3: extractedFeaturesWithActivities
+activityLabels <- data.table::fread("./UCI Har Dataset/activity_labels.txt", data.table=FALSE)[,2] #name labels corresponding to numeric labels for activities
+testActivities <- data.table::fread("./UCI Har Dataset/test/y_test.txt", data.table=FALSE) #numeric labels for activities in test set for each observation
+trainActivities <- data.table::fread("./UCI Har Dataset/train/y_train.txt", data.table=FALSE) #numeric labels for activities in train set for each observation
+activityNumbers <- rbind(testActivities, trainActivities) #merges numeric labels for training and test activities for each observation by row combining them
+activityNumbers <- activityNumbers[,1]
+activities <- vector("character")
+for(activity in activityNumbers) { #creates character vector with activity labels for each observation
+      activities <- c(activities, activityLabels[activity])
+}
+extractedFeaturesWithActivities <- cbind(Activity = activities, extractedFeatures) #adds activity labels to data frame
+
+#ANSWER TO PART 4: extractedFeaturesWithActivities
+colnames(extractedFeaturesWithActivities)[2:ncol(extractedFeaturesWithActivities)] <- features[featureIndices] #adds descriptive names to variables 
+
+#ANSWER TO PART 5
+testSubjects <- data.table::fread("./UCI Har Dataset/test/subject_test.txt", data.table=FALSE)
+trainSubjects <- data.table::fread("./UCI Har Dataset/train/subject_train.txt", data.table=FALSE)
+subjects <- rbind(testSubjects, trainSubjects)
+tidyData <- cbind(subjects, extractedFeaturesWithActivities) #adds subjects(volunteers) to data frame
+colnames(tidyData)[1] <- "Subject"
+
+#Groups data by subject and activity, then finds the mean for each variable. Arranges/sorts by subject, then activity
+tidyData <- dplyr::tbl_df(tidyData)
+tidyData <- dplyr::group_by(tidyData,Subject,Activity) %>% dplyr::summarise_all(mean) %>% dplyr::arrange(Subject,Activity)
+tidyData <- dplyr::ungroup(tidyData)
+tidyData <- as.data.frame(tidyData)
+
+write.table(tidyData,"./tidyData.txt", row.names = FALSE) #write tidy data to a txt file
+print(tidyData)
+print(ncol(tidyData))
+
